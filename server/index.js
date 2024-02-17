d680abfcb0189642ce01bf20b71bf590d198337c
tharunm1 <maddelatharun2@gmail.com>
2024-02-17 10:31:10 +0530

    inital commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    serverName: "Pro Manage",
+    status: "Active",
+  });
+});
+// ====================Error Handler===============================
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(status).json({
+    status,
+    message,
+  });
+});
+// ================================================================
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log(`server running at http://localhost:${process.env.PORT}`);
+    })
+    .catch((err) => {
+      console.log("Connection Error \n", err.message);
+    });
+});
