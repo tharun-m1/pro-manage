90d2ee822f4b20aafee7aba941c2950ef2ff54af
tharunm1 <maddelatharun2@gmail.com>
2024-07-03 08:45:34 +0530

    url updated

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,4 @@
-export const backendBaseUrl = "https://pro-manage-m9ac.onrender.com";
+export const backendBaseUrl = "https://pro-manage-8o2u.onrender.com";
+// export const backendBaseUrl = "http://localhost:4000";
 export const frontendBaseUrl = "https://magnificent-pudding-c34c83.netlify.app";
 // export const frontendBaseUrl = "http://localhost:3000";
